4bdc6bf8f838337a906085455adc1be64d2654d6
Sergey <sergeypolishchenko@gmail.com>
2025-03-21 21:17:11 +0300

    feat: add gin server for managing http requests

---
+++ internal/interfaces/http/server/server.go
@@ -0,0 +1,34 @@
+// Package httpserver provides an HTTP server implementation.
+package httpserver
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Server wraps an HTTP server instance.
+type Server struct {
+	httpServer *http.Server
+}
+
+// New creates a new HTTP server instance.
+func New(addr string, router *gin.Engine) *Server {
+	return &Server{
+		httpServer: &http.Server{
+			Addr:    addr,
+			Handler: router,
+		},
+	}
+}
+
+// Start launches the HTTP server.
+func (s *Server) Start() error {
+	return s.httpServer.ListenAndServe()
+}
+
+// Stop gracefully shuts down the server.
+func (s *Server) Stop(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
